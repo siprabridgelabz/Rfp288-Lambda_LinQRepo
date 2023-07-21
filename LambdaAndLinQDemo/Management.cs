9edb3300f981552fea62d461839dfac98fdef0d8
sipra <sipramishra085@gmail.com>
2023-07-21 18:26:36 +0530

    [Sipra]Add:Retrieve records who's rating are greater than 3

--- LambdaAndLinQDemo/Management.cs
+++ LambdaAndLinQDemo/Management.cs
@@ -18,30 +18,40 @@ namespace LambdaAndLinQDemo
             Console.WriteLine(".........");
             foreach (var prod in result)
             {
-                Console.WriteLine(prod.ProductId+""+ prod.UserId+""+ prod.Rating+""+prod.Review+""+prod.IsLike);
+                Console.WriteLine(prod.ProductId+""+ prod.UserId+""+ prod.Rating+""+
+                    prod.Review+""+prod.IsLike);
                 
             }
-            Console.WriteLine("using ");
+            Console.WriteLine("using Lambda ");
             Console.WriteLine(".........");
             foreach (var prod in result)
             {
-                Console.WriteLine(prod.ProductId + "" + prod.UserId + "" + prod.Rating + "" + prod.Review + "" + prod.IsLike);
+                Console.WriteLine(prod.ProductId + " " + prod.UserId + " " + prod.Rating 
+                    + " " + prod.Review + "" + prod.IsLike);
 
             }
         }
-        public void ProductRating(List<ProductReview>product)
+        public void ProductRating(List<ProductReview>products)
         {
-            var result = product.Where(x => x.Rating > 3 && (x.ProductId == 1 || 
+            var result = products.Where(x => x.Rating > 3 && (x.ProductId == 1 || 
             x.ProductId == 4 || x.ProductId == 9));
-            Display(result);
-           
+            var record =from product in products where (product.ProductId == 1 ||
+            product.ProductId == 4 || product.ProductId == 9) && product.Rating > 3 select product;
+
+            //Display(result);
+            foreach (var prod in result)
+            {
+                Console.WriteLine(prod.ProductId + " " + prod.UserId + " " + prod.Rating + " "
+                    + prod.Review + " " + prod.IsLike);
+            }
+
         }
         public void Display(IEnumerable<ProductReview> result)
         {
             foreach(var prod in result)
             {
-                Console.WriteLine(prod.ProductId + "" + prod.UserId + "" + prod.Rating + "" 
-                    + prod.Review + "" + prod.IsLike);
+                Console.WriteLine(prod.ProductId + " " + prod.UserId + " " + prod.Rating + " " 
+                    + prod.Review + " " + prod.IsLike);
             }
         }
     }
